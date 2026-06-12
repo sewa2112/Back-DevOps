d18c4d13f83a8c786174b9b840932d4832c3096b
Sebastian <se.osoriob@duocuc.cl>
2026-06-12 19:51:03 -0400

    confirmación pipelines

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+//Testeo epico
 const {
   DB_HOST = "10.0.2.190", // acá colocar la IP Privada EC2 DB
   DB_USER = "alumno",
